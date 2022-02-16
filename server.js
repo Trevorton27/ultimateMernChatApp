09368c36a8db7ec48962914ba8d000913883aa4d
Trevor Mearns <spiral272@gmail.com>
2022-02-16 19:23:14 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = require('express');
+const server = require('http').Server(app);
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+require('dotenv').config({ path: './config.env' });
+const connectDb = require('./serverUtilities/connectDb');
+
+connectDb();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
+app.all('*', (req, res) => handle(req, res));
+
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Express server running ${PORT}`);
+});
